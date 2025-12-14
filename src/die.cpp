bf8289e075ba039393752303751cf8e4c7cf73b5
Leonardo Bastidas <38115827+cybergoatpsyops@users.noreply.github.com>
2025-12-14 03:04:56 +0000

    Question 1

--- src/die.cpp
+++ src/die.cpp
@@ -1,1 +1,9 @@
-//
+#include "die.h"
+#include <random>
+
+int Die::roll() {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(1, sides);
+    return dis(gen);
+}
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,6 +1,16 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "die.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Die rolls return value from 1 to 6") {
+	Die die;
+	for (int i = 0; i < 10; ++i) {
+		int roll = die.roll();
+		REQUIRE(roll >= 1);
+		REQUIRE(roll <= 6);
+	}
 }
